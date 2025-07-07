a8ff1ff906a1e0c4e804f24046f290307af6d4c4
hieunn-dev <nguyenngochieu0323@gmail.com>
2025-07-07 10:55:46 +0700

    dsaf

--- app/login.jsx
+++ app/login.jsx
@@ -1,103 +1,104 @@
 import { Alert, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
 import React, { useRef, useState } from 'react'
 import ScreenWrapper from '../components/ScreenWrapper'
-import Home from '../assets/icons/Home'
+import Icon from '../assets/icons' // vì Icon đc viết trong index.jsx nên có thể import như vậy
 import { theme } from '../constants/theme'
 import { StatusBar } from 'expo-status-bar'
 import BackButton from '../components/BackButton'
 import { useRouter } from 'expo-router'
 import { hp, wp } from '../helpers/common'
 import Input from '../components/Input'
-import Icon from '../assets/icons'
 import Button from '../components/Button'
-
 const Login = () => {
-  const router = useRouter();
-  const emailRef = useRef('');
-  const passwordRef = useRef('');
-  const [loading, setLoading] = useState(false)
+    const router = useRouter();
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const [loading, setLoading] = useState(false);
 
-  const onSubmit = () => {
-    if (!emailRef.current || !passwordRef.current) {
-      Alert.alert('Login', "please fill all the fields!");
-      return;
+    const onsubmit = () => {
+        // check if email and password are filled
+        if (!emailRef.current || !passwordRef.current) {
+            Alert.alert('Login', 'Please fill all fields');
+            return;
+        }
+
     }
-  }
-  return (
-    <ScreenWrapper>
-      <StatusBar style='dark' />
-      <View style={styles.container}>
-        <BackButton router={router} />
+    return (
+        <ScreenWrapper>
+            <StatusBar style='dark' />
+            <View style={styles.container}>
+                <BackButton router={router} />
 
-        <View>
-          <Text style={styles.welcomeText}>Hey,</Text>
-          <Text style={styles.welcomeText}>Welcome Back</Text>
-        </View>
+                {/* welcome  */}
+                <View>
+                    <Text style={styles.welcomeText}>Hey</Text>
+                    <Text style={styles.welcomeText}>Welcome Back</Text>
+                </View>
 
-        <View style={styles.form}>
-          <Text style={{ fontSize: hp(1.5), color: theme.colors.text }}> Please login to continute</Text>
-          <Input
-            icon={<Icon name="mail" size={26} strokeWidth={1.6} />}
-            placeholder="Enter your email"
-            onChangeText={value => emailRef.current = value}
-          />
-          <Input
-            icon={<Icon name="lock" size={26} strokeWidth={1.6} />}
-            placeholder="Enter your password" secureTextEntry
-            onChangeText={value => passwordRef.current = value}
-          />
-          <Text style={styles.forgotPassword}>
-            Forgot Password?
-          </Text>
-          <Button title={'login'} loading={loading} onPress={onSubmit} />
-        </View>
+                {/* form  */}
+                <View style={styles.form}>
+                    <Text style={{ fontSize: hp(1.5), color: theme.colors.text }}>
+                        Please login to continue
+                    </Text>
+                    <Input icon={<Icon name='mail' size={26} strokeWidth={1.6} />}
+                        placeholder='Enter your email' onchangeText={value => { emailRef.current = value }} />
+                    <Input icon={<Icon name='lock' size={26} strokeWidth={1.6} />}
+                        placeholder='Enter your password' onchangeText={value => { passwordRef.current = value }}
+                        secureTextEntry />
+                    <Text style={styles.forgotPaswword}>
+                        Forgot Password?
+                    </Text>
+                    {/* button  */}
+                    <Button title={'Login'} loading={loading} onPress={onsubmit} />
+                </View>
 
-        <View style={styles.footer}>
-          <Text style={styles.footerText}>
-            Don't have an account?
-          </Text>
-          <Pressable onPress={()=> router.push('signUp')}>
-            <Text style={[styles.footerText, { color: theme.colors.primaryDark, fontWeight: theme.fonts.semibold }]}>Sign up</Text>
-          </Pressable>
-        </View>
-
-      </View>
-
-    </ScreenWrapper>
-  )
+                {/* footer  */}
+                <View style={styles.footer}>
+                    <Text style={styles.footerText}>
+                        Don't have an account?
+                    </Text>
+                    <Pressable onPress={() => router.push('SignUp')}>
+                        <Text style={[styles.footerText, { color: theme.colors.primaryDark, fontWeight: theme.fonts.semibold }]}>
+                            Sign up
+                        </Text>
+                    </Pressable>
+                </View>
+            </View>
+        </ScreenWrapper>
+    )
 }
 
 export default Login
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    gap: 45,
-    paddingHorizontal: wp(5),
-  },
-  welcomeText: {
-    fontSize: hp(4),
-    fontWeight: theme.fonts.bold,
-    color: theme.colors.text,
-  },
-  form: {
-    gap: 25,
-  },
-  forgotPassword: {
-    textAlign: 'right',
-    fontWeight: theme.fonts.semibold,
-    color: theme.colors.text,
-  },
-  footer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: 5,
-  },
-  footerText: {
-    textAlign: 'center',
-    color: theme.colors.text,
-    fontSize: hp(1.6)
-  }
+    container: {
+        flex: 1,
+        gap: 45,
+        paddingHorizontal: wp(5),
+    },
+    welcomeText: {
+        fontSize: hp(4),
+        fontWeight: theme.fonts.bold,
+        color: theme.colors.text,
+    },
+    form: {
+        gap: 25
+    },
+    forgotPaswword: {
+        textAlign: 'right',
+        fontWeight: theme.fonts.semibold,
+        color: theme.colors.text,
+    },
+    footer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 5,
+    },
+    footerText: {
+        textAlign: 'center',
+        color: theme.colors.text,
+        fontSize: hp(1.6),
+    }
 
 })
