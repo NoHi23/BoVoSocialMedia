c1efa45033c41db4d4080ccdd982ad66c0ae9c41
Phan Tien Manh <manhamsterdam2003@gmail.com>
2025-07-07 09:54:56 +0700

    Mạnh login signup UI

---
+++ components/BackButton.jsx
@@ -0,0 +1,24 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Icon from '../assets/icons'
+import { theme } from '../constants/theme'
+
+const BackButton = ({ size = 26, router }) => {
+    return (
+        <Pressable style={styles.button}
+            onPress={() => router.back()}>
+            <Icon name='arrowLeft' strokeWidth={2.5} size={size} color={theme.colors.text} />
+        </Pressable>
+    )
+}
+
+export default BackButton
+
+const styles = StyleSheet.create({
+    button: {
+        alignSelf: 'flex-start', // căn trái nút
+        padding: 5,
+        borderRadius: theme.radius.sm,
+        backgroundColor: 'rgba(0, 0, 0, 0.07)',
+    }
+})
